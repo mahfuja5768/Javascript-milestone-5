02c6feb7adef874a2d3c61c0b9c9f382ab68a291
mahfuja5768 <mahfujaahsanmuna@gmail.com>
2023-04-04 13:18:06 +0600

    added code

--- md-27(bank of robbery -functional)/withdraw.js
+++ md-27(bank of robbery -functional)/withdraw.js
@@ -2,13 +2,14 @@ document.getElementById('btn-withdraw').addEventListener('click',function(){
     const newWithdrawAmount = getInputFieldValueById('input-withdraw-amount');
     const previousWithdrawAmount = getElementValueById('withdraw-field');
     const previousBalanceTotal = getElementValueById('balance-field');
-    if(previousBalanceTotal < newWithdrawAmount){
-        alert(`Sorry! you don't have enough money`)
+    if(previousBalanceTotal > newWithdrawAmount){
+        const totalWithdrawAmount = previousWithdrawAmount + newWithdrawAmount;
+        setTextElementValueById('withdraw-field', totalWithdrawAmount);
+        const totalBalanceAmount = previousBalanceTotal - newWithdrawAmount;
+        setTextElementValueById('balance-field', totalBalanceAmount);
     }
     else{
-    const totalWithdrawAmount = previousWithdrawAmount + newWithdrawAmount;
-    setTextElementValueById('withdraw-field', totalWithdrawAmount);
-    const totalBalanceAmount = previousBalanceTotal - newWithdrawAmount;
-    setTextElementValueById('balance-field', totalBalanceAmount)
+        alert(`Sorry! you don't have enough money`);
+        return;
     }
 })
